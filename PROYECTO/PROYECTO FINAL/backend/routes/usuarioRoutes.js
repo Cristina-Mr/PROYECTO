8b0adf0b4dc02ba68b7ec6a7fdd1ff24de967d9c
MarioooAA <127497042+MarioooAA@users.noreply.github.com>
2025-06-12 18:20:11 +0200

    Delete PROYECTO/PROYECTO FINAL directory

--- PROYECTO/PROYECTO FINAL/backend/routes/usuarioRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuarioController = require('../controllers/usuarioController');
-
-router.post('/registro', usuarioController.registrar);
-router.post('/login', usuarioController.login);
-router.get('/perfil/:id', usuarioController.perfil);
-
-module.exports = router;
